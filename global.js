8ad6227e648f58b9492b8cdbe04966966d3498bf
itsethanmichael <142641530+itsethanmichael@users.noreply.github.com>
2025-08-05 03:54:17 -0400

    Update global.js

--- global.js
+++ global.js
@@ -1,2 +1,15 @@
 function widthHeight() { document.body.style.width = screen.innerWidth; document.body.style.height = screen.innerHeight; window.addEventListener("resize", widthHeight);}
 window.addEventListener("resize", widthHeight);
+
+function goTo(x) {
+
+  switch (x) {
+
+    case 0:
+
+      window.location.href = "https://www.youtube.com/@MonsterTamer2D";
+      break;
+      
+  }
+
+}
