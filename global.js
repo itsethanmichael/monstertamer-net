dc3b91a07f0feef44551ed08510144787047ea52
itsethanmichael <142641530+itsethanmichael@users.noreply.github.com>
2025-08-05 03:32:10 -0400

    Create global.js

---
+++ global.js
@@ -0,0 +1,2 @@
+function widthHeight() { document.body.style.width = screen.innerWidth; document.body.style.height = screen.innerHeight; window.addEventListener("resize", widthHeight);}
+window.addEventListener("resize", widthHeight);
